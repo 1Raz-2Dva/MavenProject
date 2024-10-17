b8097cc424cb25ed807e9c59001c2bc24d5f4341
Alexander Zhuravlev <warcueid@mail.ru>
2024-10-17 13:36:35 +0400

    5) Test feature work

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,41 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+class Book {
+    private String title;
+    private int year;
+
+    public Book(String title, int year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [title=" + title + ", year=" + year + "]";
+    }
+}
+
+class Student {
+    private String firstName;
+    private List<Book> books;
+
+    public Student(String firstName, List<Book> books) {
+        this.firstName = firstName;
+        this.books = books;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
@@ -23,4 +56,3 @@ public class Main {
                 () -> System.out.println("Данные не найдены."));
     }
 }
-
